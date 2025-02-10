eb28168803cce891baa14cd46b1f3b291637d473
Nathen <nathen.sample@gmail.com>
2025-02-10 15:22:44 +0000

    break everything by adding proficiency and labour cost

---
+++ src/main/java/io/github/nathensample/craftsman/enums/Proficiency.java
@@ -0,0 +1,4 @@
+package io.github.nathensample.craftsman.enums;
+
+public enum Proficiency {
+}
--- src/main/java/io/github/nathensample/craftsman/items/CompositeItem.java
+++ src/main/java/io/github/nathensample/craftsman/items/CompositeItem.java
@@ -1,5 +1,6 @@
 package io.github.nathensample.craftsman.items;
 
+import io.github.nathensample.craftsman.enums.Proficiency;
 import io.github.nathensample.craftsman.util.StringUtils;
 
 import java.util.LinkedHashMap;
@@ -10,6 +11,8 @@ public class CompositeItem extends Item {
     private final int craftProduces;
     private final Map<Item, Integer> requirements;
     private final String name;
+    private final Proficiency proficiency;
+    private final int labourCost;
 
     // Used for simplified BaseItem's in testing
     CompositeItem(String name, Map<Item, Integer> requirements) {
@@ -25,7 +28,6 @@ public class CompositeItem extends Item {
         itemStore.addToItemMap(name, this);
     }
 
-
     public CompositeItem(String name, Map<Item, Integer> requirements, int craftProduces, ItemStore itemStore) {
         this.name = StringUtils.capitalizeWords(name);
         this.requirements = requirements;
@@ -33,12 +35,19 @@ public class CompositeItem extends Item {
         itemStore.addToItemMap(name, this);
     }
 
+    public Proficiency getProficiency() {
+        return proficiency;
+    }
+
+    public int getLabourCost() {
+        return labourCost;
+    }
+
     @Override
     public String getName() {
         return name;
     }
 
-    //TODO: I hate this method
     @Override
     public Map<Item, Integer> computeRequirements() {
         Map<Item, Integer> returnList = new LinkedHashMap<>();
@@ -46,33 +55,24 @@ public class CompositeItem extends Item {
             Item requiredItem = craftingRequirement.getKey();
             int requiredQuantity = craftingRequirement.getValue();
             // If it's a base item, lets add it to the returnlist
-            if (requiredItem instanceof BaseItem)
-            {
+            if (requiredItem instanceof BaseItem) {
                 //If the requirements already contain this item
-                if (returnList.containsKey(requiredItem))
-                {
+                if (returnList.containsKey(requiredItem)) {
                     // Increase the count of how many is required
                     returnList.put(requiredItem, this.getItemRequirements().get(requiredItem) + craftingRequirement.getValue());
-                }
-                else
-                {
+                } else {
                     // Just insert the item as required
                     returnList.put(requiredItem, craftingRequirement.getValue());
                 }
-            }
-            else if (requiredItem instanceof CompositeItem)
-            {
+            } else if (requiredItem instanceof CompositeItem) {
                 Map<Item, Integer> requiredForComposite = requiredItem.computeRequirements();
                 for (Map.Entry<Item, Integer> ingredientToQuantity : requiredForComposite.entrySet()) {
                     Item itemToAddToReturnedRequirements = ingredientToQuantity.getKey();
                     Integer quantityOfItemRequired = ingredientToQuantity.getValue();
                     int craftsRequired = (int) Math.ceil((double) requiredQuantity / requiredItem.getCraftProduces());
-                    if (returnList.containsKey(ingredientToQuantity.getKey()))
-                    {
+                    if (returnList.containsKey(ingredientToQuantity.getKey())) {
                         returnList.put(itemToAddToReturnedRequirements, returnList.get(ingredientToQuantity.getKey()) + craftsRequired * quantityOfItemRequired);
-                    }
-                    else
-                    {
+                    } else {
                         returnList.put(itemToAddToReturnedRequirements, craftsRequired * quantityOfItemRequired);
                     }
                 }
